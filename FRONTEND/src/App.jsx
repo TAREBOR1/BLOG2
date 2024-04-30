7edda7599b656ae640f7a5fe3c31c3bd3452e74d
tarebor <onodaifetareborgift@gmail.com>
2024-04-30 17:02:22 +0100

    routes has been initialized

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import About from './Pages/About'
+import Home from './Pages/Home'
+import Dashboard from './Pages/Dashboard'
+import Project from './Pages/Project'
+import Signin from './Pages/Signin'
+import Signout from './Pages/Signout'
 
 const App = () => {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/dashboard' element={<Dashboard/>}/>
+    <Route path='/project' element={<Project/>}/>
+    <Route path='/signin' element={<Signin/>}/>
+    <Route path='/signout' element={<Signout/>}/>
+  </Routes>
+  </BrowserRouter>
   )
 }
 
--- FRONTEND/src/Pages/Home.jsx
+++ FRONTEND/src/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+       <h1 className="text-3xl font-bold underline">
+      Home
+    </h1>
+
     </div>
   )
 }
