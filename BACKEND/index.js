fe3a4b822f0933b0a7aca65608e66f31fb3b2c27
tarebor <onodaifetareborgift@gmail.com>
2024-04-30 17:46:33 +0100

    successfully conected to database

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,6 +1,17 @@
 const express= require('express');
 const port=3000
 const app=express()
+const mongoose=require('mongoose')
+const dotenv= require('dotenv')
+
+
+dotenv.config();
+mongoose.connect("mongodb+srv://onodaifetareborgift1:amenze@cluster0.elu2zci.mongodb.net/BLOG?retryWrites=true&w=majority&appName=Cluster0")
+.then((conn)=>{console.log('database connected successfully')})
+.catch((err)=>{
+    console.log(err)
+
+})
 
 app.listen(port,()=>{
     console.log("server is running on" + port)
