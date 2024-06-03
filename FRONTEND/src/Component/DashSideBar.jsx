6e3909ddcf3ff31e4aa98065ca3af5fdaefbe08c
tarebor <onodaifetareborgift@gmail.com>
2024-06-03 21:46:13 +0100

    side bar done

--- FRONTEND/src/Component/DashSideBar.jsx
+++ FRONTEND/src/Component/DashSideBar.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {Link, useLocation} from 'react-router-dom'
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiUser} from "react-icons/hi";
+
 
 const DashSideBar = () => {
+    const location=useLocation();
+    const [tab,setTab]=useState('')
+    useEffect(()=>{
+      const urlParams= new URLSearchParams(location.search)
+      const tabfromUrl= urlParams.get('tab');
+    if(tabfromUrl){
+      setTab(tabfromUrl)
+    }
+    },[location
+    .search])
   return (
-    <div>
-      <h1>side</h1>
-    </div>
+    <Sidebar className='w-full md:min-w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+            <Link to='/Dashboard?tab=profile'>
+            <Sidebar.Item  icon={HiUser} active={tab==='profile'}  label="User" labelColor="dark">
+            Profile
+          </Sidebar.Item>
+            </Link>
+          
+          <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'>
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
   )
 }
 
 export default DashSideBar
+
